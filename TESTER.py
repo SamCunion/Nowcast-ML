8c363ed23c77880bc4b69d6823cab66b99194799
SamCunion <195793214+SamCunion@users.noreply.github.com>
2025-07-13 20:16:14 +0100

    debugging

--- TESTER.py
+++ TESTER.py
@@ -145,5 +145,7 @@ print("Quadratic Kappa: " + str(quad_kappa))
 print("\nIntensity Counts:")
 intensity_labels = ["NonTor", "EF-0", "EF-1", "EF-2", "EF-3", "EF-4", "EF-5"]
 for i in range(7):
-    print("\n[" + intensity_labels[i] + "] Actual: " + str(summed_truths.get(i, 0)) + ", Predicted: " + str(summed_preds.get(i, 0)))
+    print("[" + intensity_labels[i] + "] Actual: " + str(summed_truths.get(i, 0)) + ", Predicted: " + str(summed_preds.get(i, 0)))
 print("\n\n\nTesting completed!")
+print(summed_preds[0])
+print(summed_truths[0])
